e1ff203f3375c6900005b8c98e2b88f97c06f41c
Olga Molkova <olgamolkova190719@gmail.com>
2024-03-13 20:38:52 +0300

    step4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,25 +2,33 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import java.io.File;
 import java.util.concurrent.Callable;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
 @Command (
-        name = "gendiff",
+        name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
 
 public class App implements Callable<Integer> {
-    @CommandLine.Option(names = {"-h", "--help"}, description = "Show this help message and exit.", required = true)
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", required = true)
     private boolean help;
-    @CommandLine.Option(names = {"-V", "--version"}, description = "Print version information and exit.", required = true)
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.", required = true)
     private boolean versionName;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "format";
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
-        System.out.println(exitCode);
+        System.exit(exitCode);
     }
-
     @Override
-    public Integer call () throws Exception {
+    public Integer call() throws Exception {
         return null;
     }
 }
