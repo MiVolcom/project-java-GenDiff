35c22fdca289e41f2464b0c95998b98473113a4f
Olga Molkova <olgamolkova190719@gmail.com>
2024-03-13 20:00:36 +0300

    step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import java.util.concurrent.Callable;
+
+@Command (
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
+    @CommandLine.Option(names = {"-h", "--help"}, description = "Show this help message and exit.", required = true)
+    private boolean help;
+    @CommandLine.Option(names = {"-V", "--version"}, description = "Print version information and exit.", required = true)
+    private boolean versionName;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.out.println(exitCode);
+    }
+
+    @Override
+    public Integer call () throws Exception {
+        return null;
     }
 }
