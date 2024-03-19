28b8a59e6acfe5295445f53c506f24de9173f8c8
Olga Molkova <olgamolkova190719@gmail.com>
2024-03-19 19:34:00 +0300

    add Coverage Badge

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -2,35 +2,46 @@ import hexlet.code.Differ;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class DifferTest {
     private final String path1 = "src/test/resources/json1.json";
     private final String path2 = "src/test/resources/json2.json";
-   // private final Path pathStylish =
-    //        Paths.get("src/test/resources/expected/Stylish").toAbsolutePath().normalize();
+    private final Path pathYaml =
+            Paths.get("src/test/resources/expected/Yaml").toAbsolutePath().normalize();
 
     private final String expected =
-           "{\n"
-           +
-            "  - follow: false\n"
-           +
-            "    host: hexlet.io\n"
-           +
-            "  - proxy: 123.234.53.22\n"
-           +
-            "  - timeout: 50\n"
-           +
-            "  + timeout: 20\n"
-           +
-            "  + verbose: true\n"
-           +
-            "}";
+            "{\n"
+                    +
+                    "  - follow: false\n"
+                    +
+                    "    host: hexlet.io\n"
+                    +
+                    "  - proxy: 123.234.53.22\n"
+                    +
+                    "  - timeout: 50\n"
+                    +
+                    "  + timeout: 20\n"
+                    +
+                    "  + verbose: true\n"
+                    +
+                    "}";
 
     @Test
     public void test1() throws Exception {
-       // var expected = Files.readString(pathStylish);
+        // var expected = Files.readString(pathStylish);
         var actual = Differ.generate(path1, path2);
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void test2() throws Exception {
+        var expected = Files.readString(pathYaml);
+        var actual = Differ.generate(path1, path2);
+        assertEquals(expected, actual);
+
+    }
 }
